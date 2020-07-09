2cd5680529a881b0ed5f78fce2bdb1dac0e63f32
saraNething <67985290+saraNething@users.noreply.github.com>
2020-07-09 16:57:13 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
-let nb = 0
 input.onButtonPressed(Button.A, function () {
-    nb = 1
-    for (let index = 0; index < 10; index++) {
-        basic.showNumber(nb)
+    for (let index = 0; index < 9; index++) {
         nb += 1
         basic.showNumber(nb)
     }
+})
+let nb = 0
+nb = 1
+basic.showNumber(nb)
+basic.forever(function () {
+	
 })
